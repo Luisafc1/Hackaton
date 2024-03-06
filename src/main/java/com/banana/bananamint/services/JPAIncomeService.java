49f12bac4d61601e05c5e57063f0e5e1122666da
Degra08 <adegracia2@dxc.com>
2024-03-06 13:42:26 +0100

    Commit AltaIngreso_3

--- src/main/java/com/banana/bananamint/services/JPAIncomeService.java
+++ src/main/java/com/banana/bananamint/services/JPAIncomeService.java
@@ -1,16 +1,26 @@
 package com.banana.bananamint.services;
 
+import com.banana.bananamint.domain.Expense;
 import com.banana.bananamint.domain.Income;
 import com.banana.bananamint.exception.IncomeExpenseException;
 import com.banana.bananamint.payload.IncomeExpenseComparison;
+import com.banana.bananamint.persistence.IncomeJPARepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
-public class JPAIncomeService implements IncomeExpenseService{
+@Service
+public class JPAIncomeService implements IncomeExpenseService {
 
+    private Logger logger = LoggerFactory.getLogger(JPAIncomeService.class);
+
+    @Autowired
+    private IncomeJPARepository incomeRepository;
 
     @Override
     public List<Income> showAllIncomes(Long idCustomer) throws IncomeExpenseException {
@@ -28,12 +38,12 @@ public class JPAIncomeService implements IncomeExpenseService{
     }
 
     @Override
-    public Income addExpense(Long idCustomer, Income income) throws IncomeExpenseException {
+    public Expense addExpense(Long idCustomer, Expense expense) throws IncomeExpenseException {
         return null;
     }
 
     @Override
-    public List<Income> showAllExpensesByDateRange(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws IncomeExpenseException {
+    public List<Expense> showAllExpensesByDateRange(Long idCustomer, LocalDate initDate, LocalDate finalDate) throws IncomeExpenseException {
         return null;
     }
 
