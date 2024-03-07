c40dbd17fd64cee46f7183683eaf6bf5ec4ead29
LuisaFc1 <luisa.fleta@gmail.com>
2024-03-07 10:27:34 +0100

    Commit AltaCuenta 5

--- src/main/java/com/banana/bananamint/controller/BananamintController.java
+++ src/main/java/com/banana/bananamint/controller/BananamintController.java
@@ -1,6 +1,10 @@
 package com.banana.bananamint.controller;
+
 import com.banana.bananamint.domain.Account;
 import com.banana.bananamint.services.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +23,15 @@ public class BananamintController {
     @Autowired
     private AccountService accountService;
 
-
+    @Operation(summary = "Add a new account", description = "Returns a persisted account")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created"),
+            @ApiResponse(responseCode = "4XX", description = "Bad request")
+    })
     @PostMapping("/{cid}")
     public ResponseEntity<Account> createAccount(
-            @RequestBody @Valid Account account,@PathVariable @Min(1) Long cid
+            @RequestBody @Valid Account account, @PathVariable @Min(1) Long cid
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(accountService.open(cid,account));
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountService.open(cid, account));
     }
 }
