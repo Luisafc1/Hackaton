3cd2eb4643f7c14ddb6e8752b1823a1aee813c99
Degra08 <adegracia2@dxc.com>
2024-03-07 10:45:49 +0100

    Commit AltaIngreso_8

--- src/main/java/com/banana/bananamint/controller/BananamintController.java
+++ src/main/java/com/banana/bananamint/controller/BananamintController.java
@@ -7,6 +7,10 @@ import com.banana.bananamint.domain.Income;
 import com.banana.bananamint.services.AccountService;
 import com.banana.bananamint.services.GoalService;
 import com.banana.bananamint.services.IncomeExpenseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,16 +24,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/bananamint")
 @Validated
+@Tag(name = "Personal Finance Service", description = "Clients Finance management API")
 public class BananamintController {
 
     @Autowired
     private IncomeExpenseService inExService;
 
+    @Operation(summary = "Create a income ", description = "Create a new income")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created"),
+            @ApiResponse(responseCode = "412", description = "Not created- incorrect data")
+    })
     @PostMapping("/income/{cid}/{aid}")
     public ResponseEntity<Income> createIncome(
-            @RequestBody @Valid Income income, @PathVariable @Min(1) Long cid,@PathVariable @Min(1) Long aid
+            @RequestBody @Valid Income income, @PathVariable @Min(1) Long cid, @PathVariable @Min(1) Long aid
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body((Income) inExService.addIncome(cid,aid,income));
+        return ResponseEntity.status(HttpStatus.CREATED).body((Income) inExService.addIncome(cid, aid, income));
     }
 
 }
